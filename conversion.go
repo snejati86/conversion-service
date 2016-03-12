ab3a8f32c01309f372644d824ffbcb1d466ededc
sinasix <snejati86@gmail.com>
2016-03-12 11:08:42 -0800

    restructring , adding json , adding gitignore.

--- conversion.go
+++ conversion.go
@@ -2,29 +2,59 @@ package main
 
 import (
 "net/http"
-	"io"
-	"strconv"
+	"fmt"
+	"encoding/json"
 )
 const (
 	ALPHA=2.20462
+	KILOGRAM="k"
+	POUND="l"
 )
-func weightHandler( w http.ResponseWriter, r *http.Request){
-	r.ParseForm()
-	var c string
-	c=r.FormValue("kg")
-	if c != "" {
-		num,_:=strconv.ParseFloat(c,64)
-		io.WriteString(w,strconv.Itoa(int(num*ALPHA)))
 
-		return
+type WeightResponse struct {
+	Unit string
+	Result string
+}
+
+func pToK(val string) WeightResponse {
+	return WeightResponse{"Kilogram",val}
+}
+
+func kToP(val string) WeightResponse {
+	return WeightResponse{"Pounds",val}
+}
+
+
+func weightHandler( w http.ResponseWriter, r *http.Request){
+	fmt.Println("GET params:", r.URL.Query());
+	wtype :=r.URL.Query().Get("type")
+	weight :=r.URL.Query().Get("val")
+	if  wtype != "" && weight != ""{
+		switch (wtype) {
+			case POUND:
+				response:= pToK(weight)
+				js, err := json.Marshal(response)
+				if err == nil {
+					w.Header().Set("Content-Type","application/json")
+					w.Write(js)
+				}else{
+					fmt.Print(err)
+				}
+			case KILOGRAM:
+				response:= kToP(weight)
+				js, err := json.Marshal(response)
+				if err == nil {
+					w.Header().Set("Content-Type","application/json")
+					w.Write(js)
+				}else{
+					fmt.Print(err)
+				}
+		default:
+			panic(nil)
+		}
+	} else{
+		http.Error(w, "bad request", 400)
 	}
-	c=r.FormValue("lbs")
-	if ( c != ""){
-		num,_:=strconv.ParseFloat(c,64)
-		io.WriteString(w,strconv.Itoa(int(num/ALPHA)))
-		return
-	}
-	http.Error(w, "bad request", 400)
 }
 
 func main (){
